08bc15ac3b502c31fbc55f1d19cc439756f1d8f1
siryakko <siryakko@gmail.com>
2016-03-03 12:13:47 +0000

    test handlebar

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 var express = require("express");
+var exphbs = require("express-handlebars");
 
 var app = express();
 
 app.use(express.static('public'));
 
+app.engine('hbs', exphbs({
+    defaultLayout: 'main';
+    extname: ".hbs"
+}));
+
+app.set('view engine','hbs');
+
 app.get("/", function(req,res,next){
-    res.send("Node at idweaver By Fred");
+    res.render('home');
 });
 
 
